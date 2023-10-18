c31fd48fc35099365557bc08e21b5ddf8f84ec05
Stas <staspronin@mail.ru>
2023-10-18 18:38:08 +0300

    работа 18.10

--- js/script.js
+++ js/script.js
@@ -181,3 +181,14 @@ document.body.addEventListener('click', event => {
   document.querySelector("header").classList.remove("open")
 });
 
+
+// карусель
+const root = document.documentElement;
+const marqueeElementsDisplayed = getComputedStyle(root).getPropertyValue("--marquee-elements-displayed");
+const marqueeContent = document.querySelector("ul.marquee-content");
+
+root.style.setProperty("--marquee-elements", marqueeContent.children.length);
+
+for(let i=0; i<marqueeElementsDisplayed; i++) {
+  marqueeContent.appendChild(marqueeContent.children[i].cloneNode(true));
+}
